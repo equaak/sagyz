895a74b5b1a90daaa39ba7696fad9ada87ea5ba5
equaak <diasyerm@gmail.com>
2024-04-14 16:16:35 +0500

    third push

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
+import Auth from './pages/auth/Auth';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout back='white'/>}>
         <Route index element={<Home />}/>
+      </Route>
+      <Route>
+        <Route path='auth' element={<Auth />} />
       </Route>
     </Routes>
   );
--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -1,9 +1,35 @@
 import './Auth.css';
+import logo from '../home/logo.svg';
 
 const Auth = () => {
     return(
         <main>
-            
+            <div className='auth-block'>
+                {/* <div className='wrapper'> */}
+                    <div className='auth-right'>
+                        <img src={logo} alt='' />
+                    </div>
+                    <div className='auth-left'>
+                        <p className='auth-title'>Регистрация</p>
+                        <p className='auth-subtitle'>Название</p>
+                        <input type='text' />
+                        <p className='auth-subtitle'>Адрес</p>
+                        <input type='text' />
+                        <p className='auth-subtitle'>БИН</p>
+                        <input type='text' />
+                        <p className='auth-subtitle'>Имя</p>
+                        <input type='text' />
+                        <p className='auth-subtitle'>Эл.почта</p>
+                        <input type='text' />
+                        <p className='auth-subtitle'>Пароль</p>
+                        <input type='text' />
+                        <div className='policy'>
+                            <p><input type='checkbox' />Я принимаю Правила пользования сервисом и Политику конфиденциальности</p>
+                        </div>
+                        <button className='auth-button'>Зарегистрироваться</button>
+                    </div>
+                {/* </div> */}
+            </div>
         </main>
     )
 }
